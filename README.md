555b76cd343768ae75862e41d66f1256797f1c1e
AkyunaAkish <akyunaakish@gmail.com>
2016-09-29 14:28:46 -0600

    initial setup completed with user routes

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ $ npm install
 $ createdb react_native_with_authentication_and_navigation
 $ touch .env
 $ echo DATABASE_URL=postgres://localhost/react_native_with_authentication_and_navigation >> .env
+$ echo SECRET=$(node -e "require('crypto').randomBytes(48, function(ex, buf) { console.log(buf.toString('hex')) });") >> .env
 $ knex migrate:latest
 $ nodemon
 ```
---
+++ api_routes/user_routes.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var router = express.Router()
+var signIn = require('./api_helpers/users/sign_in').signIn
+var signUp = require('./api_helpers/users/sign_up').signUp
+var validateUser = require('./api_helpers/users/validate_user').validateUser
+
+router.post('/signin', function(req, res, next) {
+  signIn(req.body)
+  .then(function(resolve) {
+    res.status(200).json(resolve)
+  })
+  .catch(function(reject) {
+    res.status(200).json(reject)
+  })
+})
+
+router.post('/signup', function(req, res, next) {
+  signUp(req.body)
+  .then(function(resolve) {
+    res.status(200).json(resolve)
+  })
+  .catch(function(reject) {
+    res.status(200).json(reject)
+  })
+})
+
+router.post('/validateuser', function(req, res, next) {
+  validateUser(req.body)
+  .then(function(resolve) {
+    res.status(200).json(resolve)
+  })
+  .catch(function(reject) {
+    res.status(200).json(reject)
+  })
+})
+
+module.exports = router
